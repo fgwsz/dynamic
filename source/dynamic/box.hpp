c6130d114c379f0a3448158b2fe79a3779e67a4d
fgwsz <3191000458@qq.com>
2024-07-30 02:19:08 +0800

    clean box.hpp

--- source/dynamic/box.hpp
+++ source/dynamic/box.hpp
@@ -10,56 +10,40 @@ template<typename Type__>
 class Box{
     DYNAMIC_MACRO_STATIC_ASSERT(::dynamic::is_value_type_v<Type__>);
 public:
-    Box();
-    Box(Type__ const& value);
+    Box(Type__ const& value={});
     Box(Box<Type__> const& rhs);
     Box<Type__>& operator=(Type__ const& value);
     Box<Type__>& operator=(Box<Type__> const& rhs);
     virtual ~Box();
     Type__ const& get()const;
-    bool empty()const;
-    explicit operator bool()const;
 private:
     Type__* data_;
 };
 
 template<typename Type__>
-Box<Type__>::Box(){
+Box<Type__>::Box(Type__ const& value/*={}*/){
     if constexpr(::dynamic::is_big_type_v<Type__>){
-        this->data_=nullptr;
+        this->data_=new Type__{value};
     }else{
-        new((Type__*)(&(this->data_)))Type__{};
+        new((Type__*)(&(this->data_)))Type__{value};
     }
 }
 
 template<typename Type__>
-Box<Type__>::Box(Type__ const& value)
-    :Box(){
-    this->operator=(value);
-}
-
-template<typename Type__>
 Box<Type__>::Box(Box<Type__> const& rhs)
-    :Box(){
-    this->operator=(rhs);
-}
+    :Box(rhs.get())
+{}
 
 template<typename Type__>
 Box<Type__>& Box<Type__>::operator=(Type__ const& value){
+    Type__* data=nullptr;
     if constexpr(::dynamic::is_big_type_v<Type__>){
-        if(::std::addressof(value)==this->data_){
-            return *this;
-        }
-        if(!this->data_){
-            this->data_=new Type__{value};
-        }else{
-            *(this->data_)=value;
-        }
+        data=this->data_;
     }else{
-        if(::std::addressof(value)==(Type__*)(&(this->data_))){
-            return *this;
-        }
-        *(Type__*)(&(this->data_))=value;
+        data=(Type__*)(&(this->data_));
+    }
+    if(::std::addressof(value)!=data){
+        *data=value;
     }
     return *this;
 }
@@ -69,28 +53,14 @@ Box<Type__>& Box<Type__>::operator=(Box<Type__> const& rhs){
     if(::std::addressof(rhs)==this){
         return *this;
     }
-    if constexpr(::dynamic::is_big_type_v<Type__>){
-        if(!rhs.data_){
-            if(this->data_){
-                delete this->data_;
-                this->data_=nullptr;
-            }
-            return *this;
-        }else{
-            return this->operator=(rhs);
-        }
-    }else{
-        return this->operator=(rhs);
-    }
+    return this->operator=(rhs);
 }
 
 template<typename Type__>
 Box<Type__>::~Box(){
     if constexpr(::dynamic::is_big_type_v<Type__>){
-        if(this->data_){
-            delete this->data_;
-            this->data_=nullptr;
-        }
+        delete this->data_;
+        this->data_=nullptr;
     }else{
         ((Type__*)(&(this->data_)))->~Type__();
     }
@@ -99,28 +69,10 @@ Box<Type__>::~Box(){
 template<typename Type__>
 Type__ const& Box<Type__>::get()const{
     if constexpr(::dynamic::is_big_type_v<Type__>){
-        if(this->data_){
-            return *(this->data_);
-        }else{
-            DYNAMIC_MACRO_RUNTIME_ERROR("Box is empty!");
-        }
+        return *(this->data_);
     }else{
         return *(Type__ *)(&(this->data_));
     }
-}
-
-template<typename Type__>
-bool Box<Type__>::empty()const{
-    if constexpr(::dynamic::is_big_type_v<Type__>){
-        return !this->data_;
-    }else{
-        return false;
-    }
-}
-
-template<typename Type__>
-Box<Type__>::operator bool()const{
-    return this->empty();
 }
 
 }//namespace dynamic
